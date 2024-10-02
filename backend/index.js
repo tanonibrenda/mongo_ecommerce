454d388ee9d08cf89fe1bf60d9f431745b1cd215
tanonibrenda <brendayohenatanoni@gmail.com>
2024-10-02 13:35:28 -0300

    pruebaServidorHolaMundo

--- backend/index.js
+++ backend/index.js
@@ -4,10 +4,13 @@ const server = express();
 
 const dotenv = require('dotenv');
 
+//llamar a json
+const data = require('./data.json')
+
 //primera ruta
 server.get('/', (req, res)=>{
     console.log(req)
-    res.send('<h1>Api de tienda</h1>')
+    res.send('<h1>Api de Tienda: hola mundo</h1>')
 })
 
 //servidor en puerto local
